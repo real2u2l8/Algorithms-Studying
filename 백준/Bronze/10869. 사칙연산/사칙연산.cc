bc63cc8e4d994f7fd2ddbee064fb5484b2ca42df
SEOSLE <42805229+real2u2l8@users.noreply.github.com>
2022-12-21 11:34:54 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void){
+    int a = 0;
+    int b = 0;
+
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << a + b << std::endl;
+    std::cout << a - b << std::endl;
+    std::cout << a * b << std::endl;
+    std::cout << a / b << std::endl;
+    std::cout << a % b << std::endl;
+    
+    return 0;
+}
