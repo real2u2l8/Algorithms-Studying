b3598b8f0b78031d88dd7646ad2cf2c825cce1a9
SEOSLE <real2u2l8@gmail.com>
2024-10-04 18:33:11 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+   string s;
+   int i;
+   cin >> s;
+   cin >> i;
+   cout << s.at(i-1);
+
+    return 0;
+}
+/*
+
+*/
