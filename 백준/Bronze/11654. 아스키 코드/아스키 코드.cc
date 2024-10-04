7eec53b88f48f4e3d1c6d9cad5d7c8e41ea93f78
SEOSLE <real2u2l8@gmail.com>
2024-10-04 19:14:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    cout << int(c);
+    
+    return 0;
+}
+/*
+
+*/
