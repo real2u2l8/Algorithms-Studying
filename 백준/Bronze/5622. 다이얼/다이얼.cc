db4384810496d487207244fd383fae080247b621
SEOSLE <real2u2l8@gmail.com>
2024-10-24 19:40:18 +0900

    초기에 작성한 모던한 코드 추가

--- 백준/Bronze/5622. 다이얼/다이얼.cc
+++ 백준/Bronze/5622. 다이얼/다이얼.cc
@@ -36,3 +36,51 @@ int main() {
 
     return 0;
 }
+
+/*
+// c++ STL, 함수를 최대한 사용함
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+// 1, 2[ABC], 3[DEF], 4[GHI], 5[JKL], 6[MNO], 7[PQRS], 8[TUV], 9[WXYZ], 
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    vector<tuple<int, int, string>> dial = {
+        {3, 2, "ABC"},
+        {4, 3, "DEF"},
+        {5, 4, "GHI"},
+        {6, 5, "JKL"},
+        {7, 6, "MNO"},
+        {8, 7, "PQRS"},
+        {9, 8, "TUV"},
+        {10, 9, "WXYZ"}
+    };
+    string s;
+    int sum = 0;
+    cin >> s;
+
+    for(auto& c : s){ //받은 문자를 자름
+        for(auto& data : dial){//dial 접근
+            const string& str = get<2>(data);
+            if(str.find(c) != string::npos){ //다이얼의 문자 확인
+                sum += get<0>(data);
+                break;
+            }
+        }
+    }  
+
+    cout << sum << endl;
+    return 0;
+}
+        
+
+
+
+*/
